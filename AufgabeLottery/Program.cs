5aef7a5d129a3d605d01055aea4040f9e438949c
jkueng83 <johannes.kueng@gmx.at>
2020-10-22 18:14:03 +0200

    first commit

---
+++ AufgabeLottery/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AufgabeLottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aufgabe Lottery!");
+
+            Random random = new Random();
+
+            string lotteryNumber = "";
+
+            for (int i = 0; i < 6; i++)
+            {
+                lotteryNumber += random.Next(0, 10);
+                // Console.WriteLine(lotteryNumber);
+            }
+
+            Console.WriteLine("Lottery Zahl: " + lotteryNumber);
+
+            Boolean isCorrect = false;
+
+            while (!isCorrect)
+            {
+                Console.WriteLine("Bitte geben Sie ihren Tipp ein:");
+                string input = Console.ReadLine();
+
+                int test;
+
+                if (input.Length != 6)
+                {
+                    Console.WriteLine("Ihre Eingabe ist falsch.");
+                }
+                else
+                {
+
+                    string printString = "";
+
+                    for (int i = 0; i < 6; i++)
+                    {
+                        if (lotteryNumber.Substring(i, 1) == input.Substring(i, 1))
+                        {
+                            printString += lotteryNumber.Substring(i, 1);
+                        }
+                        else
+                        {
+                            printString += "?";
+                        }
+                    }
+
+                    if (printString.Equals(lotteryNumber))
+                    {
+                        Console.WriteLine("RICHTIG: " + printString);
+                        isCorrect = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Folgende Zahlen stimmen: " + printString);
+                        Console.WriteLine("Bitte versuchen Sie es noch einmal");
+                    }
+
+                }
+
+            }
+
+
+        }
+    }
+}
